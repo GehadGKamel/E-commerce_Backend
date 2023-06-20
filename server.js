46579255ec5f7a80fe1dd3223ceeb3b7f26d6ce5
GehadGKamel <gehad.g.kamel22@gmail.com>
2023-06-20 14:08:10 +0300

    project folders structure

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-dotenv.config({ path: 'config.env' });
 
+dotenv.config({ path: 'config.env' });
+const dbConnection = require('./config/database');
+const categoryRoute = require('./routes/categoryRoute');
+
+//Connect with db
+dbConnection();
+
+//express app
 const app = express();
+
+//middlewares
+app.use(express.json());
 
 if (process.env.NODE_ENV == 'development') {
    app.use(morgan('dev'));
    console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-
-app.get('/', (req, res) => {
-   res.send('our API V3');
-})
+//Mount Routes
+app.use('/api/v1/categories',categoryRoute);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
