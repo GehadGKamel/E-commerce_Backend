0ea2ee435e6cf6cd2db0e83e190cd690044e9569
GehadGKamel <gehad.g.kamel22@gmail.com>
2023-06-20 14:27:43 +0300

    creat schema and model

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -2,10 +2,21 @@ const mongoose = require('mongoose');
 
 //1-create schema 
 const categorySchema = new mongoose.Schema({
-    name:String,
-});
+    name: {
+        type: String,
+        required: [true, 'Category required'],
+        unique: [true, 'Category must be unique'],
+        minlength: [3, 'too short category name'],
+        maxlength: [32, 'too long category name'],
+    },
+    slug: {
+        type: String,
+        lowercase: true,
+    },
+},
+    { timestamps: true });
 
 //2-create model
-const CategoryModel = mongoose.model('Category' , categorySchema);
+const CategoryModel = mongoose.model('Category', categorySchema);
 
 module.exports = CategoryModel;
