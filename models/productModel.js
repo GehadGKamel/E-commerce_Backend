68ab2d6f29cdb492632c4e071ce21cde2317d08c
GehadGKamel <gehad.g.kamel22@gmail.com>
2023-07-08 16:18:21 +0300

    product schema

---
+++ models/productModel.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [3, "too short product title"],
+      maxlength: [100, "too long product title"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: [true, "product description required"],
+      minlength: [20, "too short product description"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "product quantity required"],
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "product price required"],
+      trim: true,
+      maxlength: [10, "too long product price"],
+    },
+    priceAfterDiscount: {
+      type: Number,
+    },
+    colors: [String],
+    imageCover: {
+      type: String,
+      required: [true, "product image cover required"],
+    },
+    images: [String],
+    category: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Category",
+      required: [true, "product must belong to category "],
+    },
+    subcaregory: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "SubCategory",
+      },
+    ],
+    brand: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Brand",
+    },
+    ratingsAverage:{
+      type:Number,
+      min:[1, "Rating must be above 1.0"],
+      max:[5, "Rating must be below 5.0"],
+    },
+    ratingsQuantity:{
+      type:Number,
+      default:0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
