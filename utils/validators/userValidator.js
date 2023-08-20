a599d1d0bdea8b586ffcaa0dabedd63e4d3bae39
GehadGKamel <gehad.g.kamel22@gmail.com>
2023-08-20 19:49:58 +0300

    user validation layer

---
+++ utils/validators/userValidator.js
@@ -0,0 +1,64 @@
+/* eslint-disable prefer-promise-reject-errors */
+const slugify = require("slugify");
+const { check, body } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const User = require("../../models/userModel");
+
+exports.createUserValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("User required")
+    .isLength({ min: 3 })
+    .withMessage("too short User name")
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+
+  check("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("invalid Email")
+    .custom((val) =>
+      User.findOne({ email: val }).then((user) => {
+        if (user) {
+          return Promise.reject("email already exists");
+        }
+      })
+    ),
+
+  check("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+  check("phone")
+    .optional()
+    .isMobilePhone(["ar-EG", "ar-SA"])
+    .withMessage("invalid phone number only accepts egy and sa phone numbers"),
+  check("profileImg").optional(),
+  check("role").optional(),
+  validatorMiddleware,
+];
+
+exports.getUserValidator = [
+  check("id").isMongoId().withMessage("Id is not valid"),
+  validatorMiddleware,
+];
+
+exports.updateUserValidator = [
+  check("id").isMongoId().withMessage("invalid User id format"),
+  body("name")
+    .optional()
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+  validatorMiddleware,
+];
+
+exports.deleteUserValidator = [
+  check("id").isMongoId().withMessage("invalid User id format"),
+  validatorMiddleware,
+];
