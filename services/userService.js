307eb2fca012de72313ccd7d22cb058ba3c22ad6
GehadGKamel <gehad.g.kamel22@gmail.com>
2023-11-06 16:12:47 +0200

    hash user password on update handler

--- services/userService.js
+++ services/userService.js
@@ -1,8 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const { v4: uuidv4 } = require("uuid");
 const sharp = require("sharp");
+const bcrypt = require("bcryptjs");
 
 const factory = require("./handlersFactory");
+const ApiError = require("../utils/apiError");
 const { uploadSingleImage } = require("../middlewares/uploadImageMiddleware");
 const User = require("../models/userModel");
 
@@ -43,7 +45,42 @@ exports.createUser = factory.createOne(User);
 //@desc update specific user
 //@route PUT /api/v1/users/:id
 //@access Private
-exports.updateUser = factory.updateOne(User, "user");
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const document = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      slug: req.body.slug,
+      phone: req.body.phone,
+      email: req.body.email,
+      profileImg: req.body.profileImg,
+      role: req.body.role,
+    },
+    {
+      new: true,
+    }
+  );
+  if (!document) {
+    return next(new ApiError(`$No document for this id ${req.params.id}`, 404));
+  }
+  res.status(200).json({ data: document });
+});
+
+exports.changeUserPassword = asyncHandler(async (req, res, next) => {
+  const document = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      password: await bcrypt.hash(req.body.password, 12),
+    },
+    {
+      new: true,
+    }
+  );
+  if (!document) {
+    return next(new ApiError(`$No document for this id ${req.params.id}`, 404));
+  }
+  res.status(200).json({ data: document });
+});
 
 ////@desc delete specific user
 //@route DELETE /api/v1/users/:id
